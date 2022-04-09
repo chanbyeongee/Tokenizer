b21df6e14f47b735bb4c17192117bf1e9c462177
Chan <well87865@gmail.com>
2022-04-09 13:38:26 +0900

    Upated Konlpy

--- KoNLPy.py
+++ KoNLPy.py
@@ -1,8 +1,8 @@
-from konlpy.tag import Twitter
+from konlpy.tag import Mecab
 
 class KoNLPy:
     def __init__(self,score_sheet=None):
-        self.twitter = Twitter()
+        self.mecab = Mecab()
         self.weights = self._set_weights(score_sheet)
 
     def evaluate(self, candidate):
@@ -19,10 +19,10 @@ class KoNLPy:
         return score
 
     def analyze(self,text):
-        return self.twitter.pos(text)
+        return self.mecab.pos(text)
 
     def get_morphs(self,text):
-        return self.twitter.morphs(text)
+        return self.mecab.morphs(text)
 
     def _set_weights(self, score_sheet):
 
@@ -39,17 +39,16 @@ class KoNLPy:
         return weights
 
     def set_customized_func(self,weights,func):
-        self.twitter.set_evaluator(weights,func)
+        self.mecab.set_evaluator(weights,func)
 
     def _max_length_of_noun(self, wordpos_list):
         satisfied = [len(wordpos[0]) for wordpos in wordpos_list if wordpos[1] == 'Noun']
         return max(satisfied) if satisfied else 0
 
 if __name__ == "__main__":
-    text = "아버지가방에들어가신다."
+    text = "내가 그린 기린 그림."
     test = KoNLPy()
     raw_answer = test.analyze(text)
     print(raw_answer)
-    print(test.evaluate(raw_answer))
     print(test.get_morphs(text))
 
